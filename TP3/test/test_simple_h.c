f0ef11de1a4796631f57546830423ab996207502
Alexis Pojomovsky <apojomovsky@gmail.com>
2018-10-15 20:06:02 -0300

    Agrego un test para probar la configuracion de frecuencia PWM

--- TP3/test/test_simple_h.c
+++ TP3/test/test_simple_h.c
@@ -21,3 +21,8 @@ void test_ConfigureSimpleH_Mode(void)
   TEST_ASSERT_EQUAL(1, SimpleH_Configure_Mode());
 }
 
+void test_ConfigureSimpleH_PWM_Freq(void)
+{
+  uint8_t desiredDuty = 70;
+  TEST_ASSERT_EQUAL(716, SimpleH_SetDuty(desiredDuty));
+}
