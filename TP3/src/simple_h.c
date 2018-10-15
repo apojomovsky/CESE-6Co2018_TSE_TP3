9b0f919075a3eba50ef734afd2f9a4f0cc7179f6
Alexis Pojomovsky <apojomovsky@gmail.com>
2018-10-15 19:55:52 -0300

    Implemento la configuracion de modo SimpleH_Configure_Mode

--- TP3/src/simple_h.c
+++ TP3/src/simple_h.c
@@ -1,12 +1,22 @@
 #include "simple_h.h"
 
+#define MODE_HALF_BRIDGE 0x01
+
 static uint32_t MotorEna;
 static uint32_t MotorDir;
+static uint32_t MotorMode;
+
 
 uint8_t SimpleH_Configure_IO(void)
 {
   MotorEna = 0;
   MotorDir = 0;
+  return 1;
+}
+
+uint8_t SimpleH_Configure_Mode(void)
+{
+  MotorMode = MODE_HALF_BRIDGE;
   return 1;
 }
 
